e58d2b6949f682634aca8b3566ecf2d159fc916b
zen_vega <ufogreeks@gmail.com>
2025-09-24 16:53:01 +0200

    feat: add adc slider to keymap

--- keyboards/ccc_macropad/keymaps/zen_vega/keymap.c
+++ keyboards/ccc_macropad/keymaps/zen_vega/keymap.c
@@ -1,7 +1,13 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
-
+//
+#include "analog.h"
 #include QMK_KEYBOARD_H
+
+#define SLIDER_PIN 26
+#define SLIDER_SENSITIVITY 128
+
+static int16_t last_val = -1;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -13,7 +19,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT(KC_1, KC_2, KC_3,   // ROW1
                  KC_4, KC_5, KC_6)}; // ROW2
-                                     //
 
-//in order to read the analog input,
-//analogReadPin(GP26);
+void matrix_scan_user(void) {
+    int16_t slider_val = analogReadPin(SLIDER_PIN);
+
+    // Map to a smaller range so it’s not too sensitive
+    int16_t step = slider_val / SLIDER_SENSITIVITY;
+
+    // compare with last value and call (VOLUME_UP/VOLUME_DOWN) on change;
+    if (step != last_val) {
+        if (step > last_val) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+        last_val = step;
+    }
+}
